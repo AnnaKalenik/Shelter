d75d6337cc7a6ddaca061b0ebf162fae4a8f543c
Anna Kalenik <annakalenikalex@gmail.com>
2022-04-18 22:13:18 +0300

    feat: implement burger menu

---
+++ shelter/pages/main/script.js
@@ -0,0 +1,19 @@
+// const nav = document.querySelector('.nav');
+// const hamburger = document.querySelector('.hamburger');
+// const navLinks = document.querySelectorAll('.nav__link');
+
+// const addClassOpen = () => {
+//     hamburger.classList.toggle('hamburger_open');
+//     nav.classList.toggle('nav_open');
+//     document.body.style.overflow = 'hidden';
+// }
+// hamburger.addEventListener('click', addClassOpen);
+
+// const removeClassOpen = (event) => {
+//     if (event.target.classList.contains('nav__link')) {
+//         hamburger.classList.remove('hamburger_open');
+//         nav.classList.remove('nav_open');
+//         document.body.style.overflow = 'visible';    
+//     }
+// }
+// navLinks.forEach(link => link.addEventListener('click', removeClassOpen));
