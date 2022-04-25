9d69eb42545653712fd19e8a1100e35335218085
Anna Kalenik <annakalenikalex@gmail.com>
2022-04-25 15:43:13 +0300

    feat: implement carousel

--- shelter/assets/js/pagination.js
+++ shelter/assets/js/pagination.js
@@ -1,5 +1,3 @@
-import pets from '../../assets/js/pets.js';
-
 export default function pagination() {
     console.log('pag');
 }
--- shelter/assets/js/popup.js
+++ shelter/assets/js/popup.js
@@ -1,5 +1,3 @@
-import pets from '../../assets/js/pets.js';
-
 export default function popup() {
     console.log('ow');
 }
