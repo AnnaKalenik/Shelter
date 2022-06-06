6cc4a37466321a03c2aa5e772a17861f331e4509
Anna Kalenik <annakalenikalex@gmail.com>
2022-06-06 18:47:59 +0300

    feat: implement pagination

--- shelter/assets/js/carousel.js
+++ shelter/assets/js/carousel.js
@@ -66,12 +66,30 @@ export default function carousel() {
         let arr = [0, 1, 2, 3, 4, 5, 6, 7];
         arrFilter = [];
 
-       
-        arr.forEach(item => {
-            if (item !== arrCardsActive[0] && item !== arrCardsActive[1] && item !== arrCardsActive[2]) {
-                arrFilter.push(item);
-            }
-        })
+        if (window.innerWidth >= '1280px') {
+            arr.forEach(item => {
+                if (item !== arrCardsActive[0] && item !== arrCardsActive[1] && item !== arrCardsActive[2]) {
+                    arrFilter.push(item);
+                }
+            })
+        }
+
+        // if (window.innerWidth >= '768px' && window.innerWidth < '1280px') {
+        //     arr.forEach(item => {
+        //         if (item !== arrCardsActive[0] && item !== arrCardsActive[1]) {
+        //             arrFilter.push(item);
+        //         }
+        //     })
+        // }
+
+        // if (window.innerWidth < '768px') {
+        //     arr.forEach(item => {
+        //         if (item !== arrCardsActive[0]) {
+        //             arrFilter.push(item);
+        //         }
+        //     })
+        // }
+        
         
         console.log('Sort:', arrFilter);
         return arrFilter;
@@ -111,27 +129,76 @@ export default function carousel() {
 
         getSortArr();
 
-        if (event.animationName === 'move-left') {
-            arrCardsNew = [arrFilter[3], arrFilter[1], arrFilter[4]];
-            console.log('New:', arrCardsNew);
-        } else {
-            arrCardsNew = [arrFilter[2], arrFilter[0], arrFilter[3]];
-            console.log('New:', arrCardsNew);
+        if (window.innerWidth >= '1280px') {
+            if (event.animationName === 'move-left') {
+                arrCardsNew = [arrFilter[3], arrFilter[1], arrFilter[4]];
+                console.log('New:', arrCardsNew);
+            } else {
+                arrCardsNew = [arrFilter[2], arrFilter[0], arrFilter[3]];
+                console.log('New:', arrCardsNew);
+            }
+    
+            changeItem.innerHTML = '';
+    
+            for (let i = 0; i < 3; i++) {
+                let item = document.createElement('div');
+                item.className = `wrapper__item wrapper__item_item${i+1}`;
+    
+                item.innerHTML = `
+                    <img src="${pets[arrCardsNew[i]].img}" alt="${pets[arrCardsNew[i]].name}" class="wrapper__img">
+                    <div class="wrapper__subtitle title">${pets[arrCardsNew[i]].name}</div>
+                    <button class="wrapper__btn btn">Learn more</button>`;
+    
+                changeItem.append(item);
+            }
         }
 
-        changeItem.innerHTML = '';
+        // if (window.innerWidth >= '768px' && window.innerWidth < '1280px') {
+        //     if (event.animationName === 'move-left') {
+        //         arrCardsNew = [arrFilter[1], arrFilter[4]];
+        //         console.log('New:', arrCardsNew);
+        //     } else {
+        //         arrCardsNew = [arrFilter[0], arrFilter[3]];
+        //         console.log('New:', arrCardsNew);
+        //     }
+    
+        //     changeItem.innerHTML = '';
+    
+        //     for (let i = 0; i < 2; i++) {
+        //         let item = document.createElement('div');
+        //         item.className = `wrapper__item wrapper__item_item${i+1}`;
+    
+        //         item.innerHTML = `
+        //             <img src="${pets[arrCardsNew[i]].img}" alt="${pets[arrCardsNew[i]].name}" class="wrapper__img">
+        //             <div class="wrapper__subtitle title">${pets[arrCardsNew[i]].name}</div>
+        //             <button class="wrapper__btn btn">Learn more</button>`;
+    
+        //         changeItem.append(item);
+        //     }
+        // }
 
-        for (let i = 0; i < 3; i++) {
-            let item = document.createElement('div');
-            item.className = `wrapper__item wrapper__item_item${i+1}`;
+        // if (window.innerWidth < '768px') {
+        //     if (event.animationName === 'move-left') {
+        //         arrCardsNew = [arrFilter[randomNumber()]];
+        //         console.log('New:', arrCardsNew);
+        //     } else {
+        //         arrCardsNew = [arrFilter[randomNumber()]];
+        //         console.log('New:', arrCardsNew);
+        //     }
+    
+        //     changeItem.innerHTML = '';
+    
+        //     let item = document.createElement('div');
+        //     item.className = `wrapper__item wrapper__item_item${1}`;
+    
+        //     item.innerHTML = `
+        //         <img src="${pets[arrCardsNew[0]].img}" alt="${pets[arrCardsNew[0]].name}" class="wrapper__img">
+        //         <div class="wrapper__subtitle title">${pets[arrCardsNew[0]].name}</div>
+        //         <button class="wrapper__btn btn">Learn more</button>`;
+    
+        //     changeItem.append(item);
+        // }
 
-            item.innerHTML = `
-                <img src="${pets[arrCardsNew[i]].img}" alt="${pets[arrCardsNew[i]].name}" class="wrapper__img">
-                <div class="wrapper__subtitle title">${pets[arrCardsNew[i]].name}</div>
-                <button class="wrapper__btn btn">Learn more</button>`;
-
-            changeItem.append(item);
-        }
         console.log('-------------------------------------');
     
         addEventEventsClickButtons();
